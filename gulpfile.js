5f5627052b852f429a8e953649b4448dea42ec48
Marcos S. Camargo <mscamargo@outlook.com.br>
2019-01-06 11:30:10 -0200

    refact: structure adaptation for use with node

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,21 @@
-"use strict";
+'use strict'
 
 // Load plugins
-const autoprefixer = require("autoprefixer");
-const browsersync = require("browser-sync").create();
-const cssnano = require("cssnano");
-const gulp = require("gulp");
-const imagemin = require("gulp-imagemin");
-const plumber = require("gulp-plumber");
-const postcss = require("gulp-postcss");
-const sass = require("gulp-sass");
-const minify = require("gulp-minify");
-const babel = require("gulp-babel");
-
-// BrowserSync
-function browserSync(done) {
-  browsersync.init({
-    server: {
-      baseDir: "./dist/"
-    },
-    port: 3000
-  });
-  done();
-}
+const autoprefixer = require('autoprefixer')
+const browsersync = require('browser-sync').create()
+const cssnano = require('cssnano')
+const gulp = require('gulp')
+const imagemin = require('gulp-imagemin')
+const plumber = require('gulp-plumber')
+const postcss = require('gulp-postcss')
+const sass = require('gulp-sass')
+const minify = require('gulp-minify')
+const babel = require('gulp-babel')
 
 // Optimize Images
-function images() {
+function images () {
   return gulp
-    .src("lib/img/*")
+    .src('src/resources/assets/img/*')
     .pipe(
       imagemin([
         imagemin.gifsicle({ interlaced: true }),
@@ -42,41 +31,41 @@ function images() {
         })
       ])
     )
-    .pipe(gulp.dest("dist/assets/img"));
+    .pipe(gulp.dest('src/public/assets/img'))
 }
 
 // CSS task
-function css() {
+function css () {
   return gulp
-    .src("lib/scss/*.scss")
+    .src('src/resources/assets/scss/*.scss')
     .pipe(plumber())
-    .pipe(sass({ outputStyle: "expanded" }))
-    .pipe(gulp.dest("dist/assets/css"))
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(gulp.dest('src/public/assets/css'))
     .pipe(postcss([autoprefixer(), cssnano()]))
-    .pipe(gulp.dest("dist/assets/css"))
-    .pipe(browsersync.stream());
+    .pipe(gulp.dest('src/public/assets/css'))
+    .pipe(browsersync.stream())
 }
 
 // Transpile, concatenate and minify scripts
-function scripts() {
+function scripts () {
   return gulp
-    .src(["lib/js/*.js"])
+    .src(['src/resources/assets/js/*.js'])
     .pipe(plumber())
-    .pipe(babel({ presets: ["@babel/env"] }))
+    .pipe(babel({ presets: ['@babel/env'] }))
     .pipe(minify())
-    .pipe(gulp.dest("dist/assets/js"))
-    .pipe(browsersync.stream());
+    .pipe(gulp.dest('src/public/assets/js'))
+    .pipe(browsersync.stream())
 }
 
 // Watch files
-function watchFiles() {
-  gulp.watch("lib/scss/*.scss", css);
-  gulp.watch("lib/js/*.js", scripts);
-  gulp.watch("lib/img/*", images);
+function watchFiles () {
+  gulp.watch('src/resources/assets/scss/*.scss', css)
+  gulp.watch('src/resources/assets/js/*.js', scripts)
+  gulp.watch('src/resources/assets/img/*', images)
 }
 
 // Tasks
-gulp.task("images", images);
-gulp.task("css", css);
-gulp.task("js", scripts);
-gulp.task("default", gulp.parallel(watchFiles, browserSync));
+gulp.task('images', images)
+gulp.task('css', css)
+gulp.task('js', scripts)
+gulp.task('default', gulp.parallel(watchFiles))
